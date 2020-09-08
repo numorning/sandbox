b87ad0938fa9e7094e3f105f0495aa5139550009
David Carle <david@numorning.com>
2020-09-08 16:22:34 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Bonjour</h1>
+    <p>Bienvenue dans ce jeu</p>
+    <p>La variable nombre vaut : {{ nombre }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nombre: 12
+    }
+  }
+}
+</script>
